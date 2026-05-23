644bc4d3d7406c9383242229c2e651fa25f9bd28
Oscarmq0703 <laureldynasty@gmail.com>
2026-05-24 02:14:53 +0800

    Verify Mannes admissions requirements

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ const STATUS_CLASS = {
   verified: "verified",
 };
 
-const DATA_VERSION = "2026-05-24-msm-audit-v21";
+const DATA_VERSION = "2026-05-24-mannes-audit-v22";
 
 const state = {
   data: null,
