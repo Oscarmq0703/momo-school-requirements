07f06b064313c3e0054ef1d17cf2f3918c004ec3
Oscarmq0703 <laureldynasty@gmail.com>
2026-05-25 10:13:06 +0800

    Verify University of Cincinnati admissions requirements

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ const STATUS_CLASS = {
   verified: "verified",
 };
 
-const DATA_VERSION = "2026-05-25-university-of-arizona-audit-v41";
+const DATA_VERSION = "2026-05-25-university-of-cincinnati-audit-v42";
 
 const state = {
   data: null,
