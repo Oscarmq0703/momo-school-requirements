b12d9d15842e359d832832c1b253c22467ca164e
Oscarmq0703 <laureldynasty@gmail.com>
2026-05-25 19:13:21 +0800

    Optimize transparent banner for mobile

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ const STATUS_CLASS = {
   verified: "verified",
 };
 
-const DATA_VERSION = "2026-05-25-image-banner-v66";
+const DATA_VERSION = "2026-05-25-transparent-banner-v67";
 
 const state = {
   data: null,
