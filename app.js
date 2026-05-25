fadbfe35cd4f12d516e1ad096286fb20aa07f055
Oscarmq0703 <laureldynasty@gmail.com>
2026-05-25 17:42:03 +0800

    Verify West Virginia University admissions requirements

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ const STATUS_CLASS = {
   verified: "verified",
 };
 
-const DATA_VERSION = "2026-05-25-university-of-washington-audit-v62";
+const DATA_VERSION = "2026-05-25-west-virginia-university-audit-v63";
 
 const state = {
   data: null,
