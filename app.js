091eaf191f158d3595aca782466e4545356887b6
Oscarmq0703 <laureldynasty@gmail.com>
2026-05-25 16:06:37 +0800

    Verify University of Northern Colorado admissions requirements

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ const STATUS_CLASS = {
   verified: "verified",
 };
 
-const DATA_VERSION = "2026-05-25-university-of-north-texas-audit-v56";
+const DATA_VERSION = "2026-05-25-university-of-northern-colorado-audit-v57";
 
 const state = {
   data: null,
