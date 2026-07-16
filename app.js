568f758a063f252e6953ff16b51c5bcca902ceb4
Oscarmq0703 <laureldynasty@gmail.com>
2026-07-16 21:13:04 +0800

    Refresh first ten school requirements

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ const STATUS_CLASS = {
   verified: "verified",
 };
 
-const DATA_VERSION = "2026-05-25-new-banner-v68";
+const DATA_VERSION = "2026-07-16-first-ten-refresh-v69";
 
 const state = {
   data: null,
